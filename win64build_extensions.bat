766c043d360a38f742075037a47c5a626d95fcf3
schiffma <lorenz.schiffmann@gmail.com>
2021-06-20 21:25:17 +0200

    Update win64build_extensions.bat

--- win64build_extensions.bat
+++ win64build_extensions.bat
@@ -1,17 +1,12 @@
-:: Destination path - specify where to move package after build
-SET DEST_PATH=C:\Users\schiffma\Documents\GitHub\permutations
-
-SET SQLITE_DIR=C:\Users\schiffma\Documents\GitHub\permutations
-
-:: WIN64 SQLITE BUILD PROCEDURE
+:: Extension path - specify where this extension will be built.
+SET EXT_PATH=C:\Users\schiffma\Documents\GitHub\permutations
 
 :: Set path variables
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-CD %SQLITE_DIR%
+CD %EXT_DIR%
 
 :: Build extensions
-
 cl /MD permutations.c -link -dll -out:permutations_64.dll
 
 pause
